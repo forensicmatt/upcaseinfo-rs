8baf36226c6e13747fc817a32c48da535ac541dc
forensicmatt <matthew.seyer@gmail.com>
2020-07-15 20:39:10 -0600

    Step Four - UpcaseInfo Serializations

--- src/info.rs
+++ src/info.rs
@@ -1,11 +1,12 @@
 use std::io::{Read, Error};
+use serde::Serialize;
 use byteorder::{ReadBytesExt, LittleEndian};
 
 
 /// Reference to structure description
 /// https://github.com/vitalif/ntfs-3g/blob/04d4b37a9a6c992d89c93193d0abdd13ab1f2931/ntfsprogs/mkntfs.c#L168
 /// 
-#[derive(Debug)]
+#[derive(Serialize, Debug)]
 pub struct UpcaseInfo {
     pub len: u32,
     pub filler: u32,
@@ -33,6 +34,16 @@ impl UpcaseInfo {
                 len, filler, crc, osmajor, osminor,
                 build, packmajor, packminor
             }
+        )
+    }
+}
+
+impl std::fmt::Display for UpcaseInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f, 
+            "crc: {}\nosmajor: {}\nosminor: {}\nbuild: {}\npackmajor: {}\npackminor: {}", 
+            self.crc, self.osmajor, self.osminor, self.build, self.packmajor, self.packminor
         )
     }
 }
--- tests/test_info.rs
+++ tests/test_info.rs
@@ -1,5 +1,53 @@
 use std::io::Cursor;
+use serde_json;
 use upcaseinfo::info::UpcaseInfo;
+
+
+#[test]
+fn test_info_text() {
+    // Read file into byte slice (&'static [u8; N])
+    let info_buffer = include_bytes!("../testdata/Info");
+    // Create a curser for the byte slice so Read is implemented
+    let info_buffer_cursor = Cursor::new(info_buffer);
+
+    // Pass the cursor to the UpcaseInfo::new to parse out the upcase info struct
+    let upcase_info = UpcaseInfo::new(info_buffer_cursor)
+        .expect("Error parsing Upcase:Info buffer.");
+    
+    // Serialize it to a JSON string.
+    let upcase_as_text = upcase_info.to_string();
+
+    assert_eq!(
+        upcase_as_text, 
+        "crc: 15770619046507800844\nosmajor: 10\nosminor: 0\nbuild: 16299\npackmajor: 0\npackminor: 0"
+    );
+
+    println!("UpcaseInfo text -> {}", upcase_as_text);
+}
+
+
+#[test]
+fn test_info_json() {
+    // Read file into byte slice (&'static [u8; N])
+    let info_buffer = include_bytes!("../testdata/Info");
+    // Create a curser for the byte slice so Read is implemented
+    let info_buffer_cursor = Cursor::new(info_buffer);
+
+    // Pass the cursor to the UpcaseInfo::new to parse out the upcase info struct
+    let upcase_info = UpcaseInfo::new(info_buffer_cursor)
+        .expect("Error parsing Upcase:Info buffer.");
+    
+    // Serialize it to a JSON string.
+    let upcase_json = serde_json::to_string(&upcase_info)
+        .expect("Error serializing upcase_info into JSON");
+
+    assert_eq!(
+        upcase_json, 
+        r#"{"len":32,"filler":0,"crc":15770619046507800844,"osmajor":10,"osminor":0,"build":16299,"packmajor":0,"packminor":0}"#
+    );
+
+    println!("UpcaseInfo json -> {}", upcase_json);
+}
 
 
 #[test]
